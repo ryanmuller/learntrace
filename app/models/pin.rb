0d6fe68b6bf55d3c7c70fc682fb436a52ed22a66
Ryan Muller <cognitionmachine@gmail.com>
2012-07-10 18:10:33 -0400

    featured scope to order items within a tag by the number of pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,7 @@ class Pin < ActiveRecord::Base
   attr_accessible :item_id
 
   belongs_to :user
-  belongs_to :item
+  belongs_to :item, :counter_cache => true
 
   validates :user_id, presence: true
   validates :item_id, presence: true
