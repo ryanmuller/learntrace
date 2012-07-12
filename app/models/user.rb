b0a230888a16d782d30457446f33876a0fae9d8b
Ryan Muller <cognitionmachine@gmail.com>
2012-07-12 14:16:32 -0400

    weird shit

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,12 +45,12 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(name: auth.extra.raw_info.name,
-                         provider: auth.provider,
+      user = User.create(provider: auth.provider,
                          uid: auth.uid,
                          email: auth.info.email,
                          password: Devise.friendly_token[0,20])
     end
+    return user
   end
 
 end
