a45f2f2210bbb788a77250d603231b5be45dbc40
Ryan Muller <cognitionmachine@gmail.com>
2012-07-11 23:15:57 -0400

    another facebook fix hopefully

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,12 +42,14 @@ class User < ActiveRecord::Base
     pins.find_by_item_id(item)
   end
 
-  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-    data = access_token['extra']['user_hash']
-    if user = User.find_by_email(data["email"])
-      user
-    else # Create a user with a stub password.
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name: auth.extra.raw_info.name,
+                         provider: auth.provider,
+                         uid: auth.uid,
+                         email: auth.info.email,
+                         password: Devise.friendly_token[0,20])
     end
   end
 
