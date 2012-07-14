e20793c586a6fc4d8f24168333665f7d8d5ff175
Ryan Muller <cognitionmachine@gmail.com>
2012-07-13 23:16:23 -0400

    model and placeholder for comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120715210056) do
+ActiveRecord::Schema.define(:version => 20120714030221) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -90,7 +90,6 @@ ActiveRecord::Schema.define(:version => 20120715210056) do
     t.string   "uid"
     t.string   "provider"
     t.string   "image"
-    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
