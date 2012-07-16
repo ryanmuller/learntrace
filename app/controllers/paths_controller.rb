9545f2bc0d86261b956794d84e84bfbae9dfbb54
Ryan Muller <cognitionmachine@gmail.com>
2012-07-16 15:03:38 -0400

    form and actions for creating paths

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -4,6 +4,24 @@ class PathsController < ApplicationController
 
   def index
     @paths = current_user.paths
+    @path = current_user.paths.build
+    @tag_data = Tag.all.map{|t| t.name }.join(",")
   end
 
+  def create
+    @path = current_user.paths.build(params[:path])
+    respond_to do |format|
+      if @path.save
+        format.html { redirect_to @path, notice: "You're ready to start learning!" }
+        format.json { render json: @path, status: :created, location: @path }
+      else
+        format.html { render action: "index" }
+        format.json { render json: @path.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @path = Path.find(params[:id]) 
+  end
 end
