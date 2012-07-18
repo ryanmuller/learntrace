3f086dedf9a76ed57c1f6af91c7d8f27e5a6ab4c
Neal Pisenti <npisenti@gmail.com>
2012-07-18 11:04:40 -0400

    rake migrate

--- db/schema.rb
+++ db/schema.rb
@@ -102,9 +102,11 @@ ActiveRecord::Schema.define(:version => 20120717183030) do
     t.string   "provider"
     t.string   "image"
     t.string   "name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username"
 
 end
