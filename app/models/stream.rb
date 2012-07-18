118249ee622fb6a49f80d40a314ca36743891a10
Ryan Muller <cognitionmachine@gmail.com>
2012-07-18 10:19:51 -0400

    model for forks that hopefully works

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -2,6 +2,8 @@ class Stream < ActiveRecord::Base
   belongs_to :user
   has_many :pins
   has_many :items, :through => :pins
+  has_many :forks, foreign_key: "target_id", dependent: :destroy
+  has_many :sources, through: :forks, source: :source
 
   validates :name, :presence => true
 end
