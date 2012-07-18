3f6fa87cdf14a0f122c9755916acd87a818957e1
Ryan Muller <cognitionmachine@gmail.com>
2012-07-18 10:19:51 -0400

    model for forks that hopefully works

---
+++ app/models/fork.rb
@@ -0,0 +1,9 @@
+class Fork < ActiveRecord::Base
+  attr_accessible :source_id
+
+  belongs_to :source, class_name: "Stream"
+  belongs_to :target, class_name: "Stream"
+
+  validates :source_id, presence: true
+  validates :target_id, presence: true
+end
