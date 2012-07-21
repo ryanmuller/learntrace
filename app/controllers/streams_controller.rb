d6943528043b27bb20202c8108e7ae5bee05a808
Ryan Muller <cognitionmachine@gmail.com>
2012-07-21 10:20:06 -0400

    ignore non scheduled items for timeline

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -42,6 +42,8 @@ class StreamsController < ApplicationController
       @suggested = @tag.items.featured
     end
 
+    @pins = @stream.pins.timeline
+
     respond_to do |format|
       format.html
       format.json 
