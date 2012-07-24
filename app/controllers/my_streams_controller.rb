8593de3b81d5f6b433e48918836c264d67498c99
Ryan Muller <cognitionmachine@gmail.com>
2012-07-24 11:28:25 -0400

    removed recommendations, other cleanup

--- app/controllers/my_streams_controller.rb
+++ app/controllers/my_streams_controller.rb
@@ -3,10 +3,6 @@ class MyStreamsController < ApplicationController
   def show
     @stream = current_user.streams.find(params[:id]) 
     @item = Item.new
-    @tag = Tag.find_by_name(@stream.name.downcase)
-    if @tag
-      @suggested = @tag.items.featured
-    end
 
     @pins = @stream.pins.timeline
 
