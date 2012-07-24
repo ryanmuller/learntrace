d14065f15dc0a89e7bd5912be80b898904c79da5
Ryan Muller <cognitionmachine@gmail.com>
2012-07-24 19:56:32 -0400

    fixed error in add forks count

--- db/migrate/20120724190430_add_forks_count.rb
+++ db/migrate/20120724190430_add_forks_count.rb
@@ -2,8 +2,8 @@ class AddForksCount < ActiveRecord::Migration
   def up
     add_column :streams, :forks_count, :integer, :default => 0
     Stream.reset_column_information
-    Stream.all.each do |s|
-      s.update_attribute :forks_count, s.forks.length
+    Stream.find_each do |s|
+      Stream.reset_counters s.id, :forks
     end
   end
 
