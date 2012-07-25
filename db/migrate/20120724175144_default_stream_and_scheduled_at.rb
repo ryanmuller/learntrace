f7c91f48bcb1dfe53ee9ade7cc2c144f0cfd15b1
Ryan Muller <cognitionmachine@gmail.com>
2012-07-25 13:23:58 -0400

    destroy pins for nil users in migration

--- db/migrate/20120724175144_default_stream_and_scheduled_at.rb
+++ db/migrate/20120724175144_default_stream_and_scheduled_at.rb
@@ -1,9 +1,15 @@
 class DefaultStreamAndScheduledAt < ActiveRecord::Migration
   def up
+
     Pin.all.each do |pin|
       unless pin.stream
         item = pin.item
         user = pin.user
+        if user.nil?
+          pin.destroy
+          next
+        end
+
         stream = user.streams.find_by_name("Unsorted items") || Stream.create!(user: user, name: "Unsorted items")
         user.unpin!(item)
         pin = user.pin!(item, stream)
